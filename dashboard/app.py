cce4af46b3e5820f0a839ebf0dd80c65e937e638
itzMonisha <monishamp15@gmail.com>
2026-06-19 22:32:09 +0530

    Fix dashboard API URL

--- dashboard/app.py
+++ dashboard/app.py
@@ -21,7 +21,7 @@ st.set_page_config(
 
 st.title("🚀 IntelliStream Dashboard")
 
-API_URL = "https://intellistream-dashboard.onrender.com/"
+API_URL = "https://intellistream.onrender.com"
 
 # --------------------------------------------------
 # ANALYTICS
