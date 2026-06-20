cc1abdb619919bc0b47a4db7d81f110d4db10404
itzMonisha <monishamp15@gmail.com>
2026-06-20 21:28:48 +0530

    Use Render database

--- api/database.py
+++ api/database.py
@@ -1,5 +1,7 @@
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://admin:admin123@postgres:5432/intellistream"
+DATABASE_URL = os.getenv(
+    "postgresql://intellistream_1_postgress_user:dg8iIlWtkRRrAUWN8O2NLz2ThVarmZW6@dpg-d8rbfomgvqtc73ertcv0-a.virginia-postgres.render.com/intellistream_1_postgress")
 
 engine = create_engine(DATABASE_URL)
